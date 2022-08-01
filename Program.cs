42f79d549940d412751c5d31b22540eafff278eb
Leandro Di Nardo Lazarin <leandrodnl09@gmail.com>
2022-08-01 15:58:10 -0300

    SomaImparesConsecitivos

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// Exercício do site URI "Soma impares consecutivos"
+
+
+int Troca, Soma;
+int X = int.Parse(Console.ReadLine());
+int Y = int.Parse(Console.ReadLine());
+
+Soma = 0;
+
+if (X > Y)
+{
+    Troca = X;
+    X = Y;
+    Y = Troca;
+}
+for (int i = X + 1; i < Y; i++)
+{
+    if (i % 2 != 0)
+    {
+        Soma = Soma + i;
+    }
+}
+Console.WriteLine(Soma);
